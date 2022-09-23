5dc061198a709956de84d3f1a524498b06e8b5a9
MartinStrekelj <martin.strekelj123@gmail.com>
2022-09-23 17:21:53 +0200

    feat: add express server

--- index.js
+++ index.js
@@ -1,55 +1,67 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import { Client, EmbedBuilder, GatewayIntentBits } from "discord.js";
-import selectRandomFlexitGif from "./src/random-flexit-gif.js";
-import anonimniName from "./src/anonimni-name.js";
+import express from "express";
+import bot from "./bot.js";
 
-const isDevMode = process.env.NODE_ENV === "dev";
+const app = express();
 
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildMessageTyping,
-    GatewayIntentBits.GuildMessageReactions,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.DirectMessages,
-    GatewayIntentBits.MessageContent,
-  ],
+const port = process.env.PORT || 3001;
+
+app.get("/", (req, res) => res.type("html").send(html));
+
+app.listen(port, () => {
+  bot.client.login(process.env.DISCORD_TOKEN);
+  console.log(`Example app listening on port ${port}!`);
 });
 
-client.login(process.env.DISCORD_TOKEN);
-
-client.on("ready", async () => {
-  console.log("Bot ready!");
-});
-
-client.on("messageCreate", async (message) => {
-  if (message.author.bot) {
-    return;
-  }
-
-  const hasMentionedFlexit = message.mentions.roles.some((mentioned_role) => mentioned_role.name.includes("League"));
-  const only4ofAkind = isDevMode ? message.author.username === "4ofAkind" : true;
-
-  if (only4ofAkind && hasMentionedFlexit) {
-    const gif = selectRandomFlexitGif.execute();
-    const response = new EmbedBuilder().setTitle("FLEXIT").setImage(gif);
-    message.channel.send({ embeds: [response] });
-  }
-
-  if (message.content === "*name random") {
-    const guild = client.guilds.cache.find((guild) => guild.id === message.guildId);
-    if (!guild) return;
-
-    const member = guild.members.cache.find((member) => member.id === message.author.id);
-    if (!member) return;
-
-    await anonimniName.execute(member, true);
-  }
-});
-
-client.on("guildMemberAdd", async (member) => {
-  await anonimniName.execute(member);
-});
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Flexit bot is online!</title>
+    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
+    <script>
+      setTimeout(() => {
+        confetti({
+          particleCount: 100,
+          spread: 70,
+          origin: { y: 0.6 },
+          disableForReducedMotion: true
+        });
+      }, 500);
+    </script>
+    <style>
+      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
+      @font-face {
+        font-family: "neo-sans";
+        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
+        font-style: normal;
+        font-weight: 700;
+      }
+      html {
+        font-family: neo-sans;
+        font-weight: 700;
+        font-size: calc(62rem / 16);
+      }
+      body {
+        background: white;
+      }
+      section {
+        border-radius: 1em;
+        padding: 1em;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%);
+      }
+    </style>
+  </head>
+  <body>
+    <section>
+      Flexit bot is online!
+    </section>
+  </body>
+</html>
+`;
